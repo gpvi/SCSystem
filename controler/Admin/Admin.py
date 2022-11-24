451fcf25fd87b1bfb198c3526d0b7dcb887b8943
gpvi <73032307+gpvi@users.noreply.github.com>
2022-11-24 14:37:18 +0800

    add funcions

---
+++ controler/Admin/Admin.py
@@ -0,0 +1,6 @@
+"""
+@FileName：Admin.py\n
+@Description：\n
+@Author：NZQ\n
+@Time：2022/11/24 14:14\n
+"""
---
+++ static/databaseInit/__init__.py
@@ -0,0 +1,6 @@
+"""
+@FileName：__init__.py.py\n
+@Description：\n
+@Author：NZQ\n
+@Time：2022/11/24 14:15\n
+"""
