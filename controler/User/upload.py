8fc9c7c8e0195fb52afa750aa6c810fbd1f62198
gpvi <73032307+gpvi@users.noreply.github.com>
2022-11-27 23:52:49 +0800

    登录注册页面

---
+++ controler/User/upload.py
@@ -0,0 +1,11 @@
+
+
+from flask import Flask,request
+import os
+
+
+app = Flask(__name__)
+app.config['']
+@app.route('/upload')
+def uploadfile():
+    
--- controler/login.py
+++ controler/login.py
@@ -4,3 +4,38 @@
 @Author：NZQ\n
 @Time：2022/11/23 20:10\n
 """
+
+
+from flask import Flask,request,url_for,redirect,send_file
+import json
+import requests
+
+
+
+login = Flask(__name__,template_folder="../static/html")
+
+@login.route("/login",methods= ['POST'])
+def func():
+    data = request.form
+    # print(data)
+    # print(data['id'])
+    # data = json.loads(data)
+    try:
+        username = data['id']
+        password = data['password']
+        print("登录成功！！")
+        return send_file("../static/html/myclasses.html")
+    except:
+        print("error")
+        return {"success":0}
+    
+@login.route("/signin",methods = ['POST'])
+
+
+
+
+
+if __name__ == '__main__':
+    login.run(port = 9090,debug = True)
+    
+    
